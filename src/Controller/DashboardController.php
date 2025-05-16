317583312560ece929a80246d82e2b5ec8c76c32
ricoi <ricoisdenaam@gmail.com>
2025-05-17 00:16:30 +0200

    Setup the DashboardController.php to test if it connects to the service.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Service\AgeVerifierApi;
+use App\Service\BanwatchApi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/', name: 'app_dashboard')]
     public function index(): Response
     {
+
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'banwatch' => (new BanwatchApi())->send_request('/ping', method: 'POST'),
+            'ageverifier' => (new AgeVerifierApi())->send_request('/ping', method: 'POST'),
         ]);
     }
 }
